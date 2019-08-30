bae0f9497e63f7214a4a6b961da582240d128316
swahn <swahn@protonmail.com>
2019-08-30 11:45:59 -0400

    responsive design

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -6,11 +6,13 @@ import TermsCA from './components/TermsCA'
 import PrivacyPolicy from './components/PrivacyPolicy'
 import ContactUs from './components/ContactUs'
 
-
 function AppRouter() {
   return (
     <Router>
       <Switch>
+        <Route exact path={config.routes.landing} render={() => 
+          <Redirect to={config.routes.terms_us} /> 
+        } />
         <Route exact path={config.routes.terms_us} component={TermsUS} />
         <Route exact path={config.routes.terms_ca} component={TermsCA} />
         <Route exact path={config.routes.privacy_policy} component={PrivacyPolicy} />
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const config = {
   routes: {
+    landing: '/',
     terms_us: '/terms-us',
     terms_ca: '/terms-ca',
     privacy_policy: '/privacy-policy',
